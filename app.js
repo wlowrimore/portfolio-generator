62b79c83dbea1fae4ce907e1cc2fc00e31feda7f
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-02-04 11:26:38 -0600

    trying a fix

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+console.log(inquirer);
+// const fs = require('fs');
+// const generatPage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // const printProfileData = profileDataArr => {
 //   for (let i = 0; i < profileDataArr.length; i += 1) {
---
+++ page-template.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+
+console.log(inquirer);
+// const fs = require('fs');
+// const generatPage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
